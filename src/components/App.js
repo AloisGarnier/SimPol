146fdfc77967f0f033b8a602e5895088332f5780
alois_garnier <alois.garnier@hotmail.com>
2023-05-31 17:01:47 +0200

    init

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+import {nanoid} from "nanoid";
+import "whatwg-fetch";
+
+import "../css/morph.min.css";
+import "../css/fontawesome.all.min.css";
+import "../css/myStyle.css";
+
+
+export default function App(){
+
+    return(
+        <div>
+        </div>
+    );
+
+}
